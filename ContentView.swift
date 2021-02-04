2f8c0b7a95cf71753ed98ba2dd83c8848e820d52
hideq <hs.show3@gmail.com>
2021-02-04 19:29:29 +0900

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,118 @@
+//
+//  ContentView.swift
+//  Slot Game
+//
+//  Created by 嶋秀憲 on 2020/09/05.
+//  Copyright © 2020 Hidenori Shima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var symbols = ["apple", "star", "cherry"]
+    @State private var numbers = [1, 2, 0]
+    @State private var backgrounds = [Color.white, Color.white, Color.white]
+    @State private var credits = 1000
+    private var betAmount = 5
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255))
+                .edgesIgnoringSafeArea(.all)
+            Rectangle()
+                .foregroundColor(Color(red: 228/255, green: 195/225, blue: 76/255))
+                .rotationEffect(Angle(degrees: 45))
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                
+                Spacer()
+                
+                // Title
+                HStack {
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    Text("SwiftUI Slots")
+                        .bold()
+                        .foregroundColor(.white)
+                    Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+                
+                }.scaleEffect(2)
+                
+                Spacer()
+                
+                // Credits counter
+                Text("Credits: " + String(credits))
+                    .foregroundColor(.black)
+                    .padding(.all, 10)
+                    .background(Color.white.opacity(0.5))
+                .cornerRadius(20)
+                
+                Spacer()
+                
+                // Cards
+                HStack {
+                    Spacer()
+                    
+                    CardView(symbol: $symbols[numbers[0]], background: $backgrounds[0])
+                    
+                    CardView(symbol: $symbols[numbers[1]], background: $backgrounds[1])
+                    
+                    CardView(symbol: $symbols[numbers[2]], background: $backgrounds[2])
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                // Button
+                Button(action: {
+                    // Set backgrounds back to white
+                    self.backgrounds[0] = Color.white
+                    self.backgrounds[1] = Color.white
+                    self.backgrounds[2] = Color.white
+                    
+                    // Change the images
+                    self.numbers[0] = Int.random(in: 0...self.symbols.count - 1)
+                    self.numbers[1] = Int.random(in: 0...self.symbols.count - 1)
+                    self.numbers[2] = Int.random(in: 0...self.symbols.count - 1)
+                    
+                    //Chack winnings
+                    if self.numbers[0] == self.numbers[1] && self.numbers[1] == self.numbers[2] {
+                        
+                        // Won
+                        self.credits += self.betAmount * 10
+                        
+                        // Updata backgrounds to green
+                        self.backgrounds[0] = Color.green
+                        self.backgrounds[1] = Color.green
+                        self.backgrounds[2] = Color.green
+                    }
+                    else {
+                        self.credits -= self.betAmount
+                    }
+                    
+            }) {
+                    Text("Spin")
+                    .bold()
+                        .foregroundColor(.white)
+                        .padding(.all, 10)
+                        .padding([.leading, .trailing], 30)
+                        .background(Color.pink)
+                    .cornerRadius(20)
+                }
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
